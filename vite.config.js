fe9bedcde758950aa173942b3deaa7dd389a437f
memar9229-droid <memar9229@gmail.com>
2026-05-06 17:58:41 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // این خط باعث می‌شود آدرس‌ها در ورسل درست لود شوند
+  base: './', 
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
