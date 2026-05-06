2e3ea4f07e398f2e0b535d2271f733d35dcbe302
memar9229-droid <memar9229@gmail.com>
2026-05-06 18:59:23 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // چون فایل index.html در کنار این فایل است، نیاز به تنظیمات پیچیده نیست
   base: '/',
   build: {
     outDir: 'dist',
