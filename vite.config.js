ba08219660e00b9713d410761af5f19627652fcf
memar9229-droid <memar9229@gmail.com>
2026-05-06 18:51:07 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  // چون فایل index.html و App.jsx هر دو در ریشه اصلی هستند، 
+  // تنظیمات زیر کمک می‌کند تا آدرس‌دهی‌ها بدون مشکل انجام شود.
+  base: '/', 
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+  },
 })
