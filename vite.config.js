328ff3522787ec1b39e9977fff6e1921d272ae49
memar9229-droid <memar9229@gmail.com>
2026-05-06 19:04:15 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: 'index.html'
-    }
+  },
+  server: {
+    port: 3000
   }
 })
