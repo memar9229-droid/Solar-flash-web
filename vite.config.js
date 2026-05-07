0eadfa03dc8efe6b9de6c018b3b36c882fcdb49b
memar9229-droid <memar9229@gmail.com>
2026-05-06 22:12:35 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          motion: ['framer-motion'],
+        }
+      }
+    }
   }
 })
