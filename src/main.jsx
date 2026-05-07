14feb0358ee9113c710dc476cad73fbaafbb59d0
memar9229-droid <memar9229@gmail.com>
2026-05-06 23:17:39 -0400

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import SolarFlashFull from './SolarFlashFull.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <SolarFlashFull />
-  </React.StrictMode>
-)
