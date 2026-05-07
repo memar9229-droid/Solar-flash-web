7f88c2c0e76d85c5a80d26de2c0bdb6120824dea
memar9229-droid <memar9229@gmail.com>
2026-05-06 23:11:29 -0400

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SolarFlashFull from './SolarFlashFull.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SolarFlashFull />
+  </React.StrictMode>
+)
