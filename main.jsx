a50cf8ff8ca0508ea6ebc0b562c4dcd7ed9dac2d
memar9229-droid <memar9229@gmail.com>
2026-05-25 11:28:37 -0400

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import SolarFlashFull from './SolarFlashFull.jsx'
-import ReportCard from './src/ReportCard.jsx'
+import ReportCard from './ReportCard.jsx'
+import Dashboard from './Dashboard.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SolarFlashFull />} />
-        <Route path="/report" element={<ReportCard />} />
+        <Route path="/"          element={<SolarFlashFull />} />
+        <Route path="/report"    element={<ReportCard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
