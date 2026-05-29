8ecb148d292a2cef1f91781955ceb22460e89bd9
memar9229-droid <memar9229@gmail.com>
2026-05-29 19:41:39 -0400

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -5,15 +5,17 @@ import SolarFlashFull from './SolarFlashFull.jsx'
 import ReportCard from './src/ReportCard.jsx'
 import Dashboard from './src/Dashboard.jsx'
 import Litepaper from './src/Litepaper.jsx'
+import SmartAlerts from './src/SmartAlerts.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/"           element={<SolarFlashFull />} />
-        <Route path="/report"     element={<ReportCard />} />
-        <Route path="/dashboard"  element={<Dashboard />} />
-        <Route path="/litepaper"  element={<Litepaper />} />
+        <Route path="/"          element={<SolarFlashFull />} />
+        <Route path="/report"    element={<ReportCard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/litepaper" element={<Litepaper />} />
+        <Route path="/alerts"    element={<SmartAlerts />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
