cde913be6865076a12f535f3250a994d69f10b83
memar9229-droid <memar9229@gmail.com>
2026-06-01 15:09:46 -0400

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -2,22 +2,24 @@
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import SolarFlashFull from './SolarFlashFull.jsx'
-import ReportCard from './src/ReportCard.jsx'
-import Dashboard from './src/Dashboard.jsx'
-import Litepaper from './src/Litepaper.jsx'
-import SmartAlerts from './src/SmartAlerts.jsx'
-import ReactorDemo from './src/SurvivalReactor.jsx'
-<Route path="/reactor-demo" element={<ReactorDemo />} />
+import ReportCard    from './src/ReportCard.jsx'
+import Dashboard     from './src/Dashboard.jsx'
+import Litepaper     from './src/Litepaper.jsx'
+import SmartAlerts   from './src/SmartAlerts.jsx'
+import Ecosystem     from './src/Ecosystem.jsx'
+import ReactorDemo   from './src/SurvivalReactor.jsx'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/"          element={<SolarFlashFull />} />
-        <Route path="/report"    element={<ReportCard />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/litepaper" element={<Litepaper />} />
-        <Route path="/alerts"    element={<SmartAlerts />} />
-      <Route path="/reactor-demo" element={<ReactorDemo />} />
+        <Route path="/"            element={<SolarFlashFull />} />
+        <Route path="/report"      element={<ReportCard />} />
+        <Route path="/dashboard"   element={<Dashboard />} />
+        <Route path="/litepaper"   element={<Litepaper />} />
+        <Route path="/alerts"      element={<SmartAlerts />} />
+        <Route path="/ecosystem"   element={<Ecosystem />} />
+        <Route path="/reactor-demo" element={<ReactorDemo />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
