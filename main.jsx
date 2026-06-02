dd5c15ec38b16740ea49bc56a97681d4e007e407
memar9229-droid <memar9229@gmail.com>
2026-06-01 22:19:44 -0400

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,26 +1,28 @@
     import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import SolarFlashFull   from './SolarFlashFull.jsx'
-import ReportCard        from './src/ReportCard.jsx'
-import Dashboard         from './src/Dashboard.jsx'
-import Litepaper         from './src/Litepaper.jsx'
-import SmartAlerts       from './src/SmartAlerts.jsx'
-import Ecosystem         from './src/Ecosystem.jsx'
-import SurvivalScorePage from './src/OrbitMetrics.jsx'
-import ReactorDemo       from './src/SurvivalReactor.jsx'
+import SolarFlashFull    from './SolarFlashFull.jsx'
+import ReportCard         from './src/ReportCard.jsx'
+import Dashboard          from './src/Dashboard.jsx'
+import Litepaper          from './src/Litepaper.jsx'
+import SmartAlerts        from './src/SmartAlerts.jsx'
+import Ecosystem          from './src/Ecosystem.jsx'
+import SurvivalScorePage  from './src/OrbitMetrics.jsx'
+import ThreatScannerPage  from './src/ThreatScanner.jsx'
+import ReactorDemo        from './src/SurvivalReactor.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/"            element={<SolarFlashFull />} />
-        <Route path="/report"      element={<ReportCard />} />
-        <Route path="/dashboard"   element={<Dashboard />} />
-        <Route path="/litepaper"   element={<Litepaper />} />
-        <Route path="/alerts"      element={<SmartAlerts />} />
-        <Route path="/ecosystem"   element={<Ecosystem />} />
-        <Route path="/score"       element={<SurvivalScorePage />} />
+        <Route path="/"             element={<SolarFlashFull />} />
+        <Route path="/report"       element={<ReportCard />} />
+        <Route path="/dashboard"    element={<Dashboard />} />
+        <Route path="/litepaper"    element={<Litepaper />} />
+        <Route path="/alerts"       element={<SmartAlerts />} />
+        <Route path="/ecosystem"    element={<Ecosystem />} />
+        <Route path="/score"        element={<SurvivalScorePage />} />
+        <Route path="/threat"       element={<ThreatScannerPage />} />
         <Route path="/reactor-demo" element={<ReactorDemo />} />
       </Routes>
     </BrowserRouter>
