42d7a8d3c83a28224a2af5e4f67c6e0b23534af0
memar9229-droid <memar9229@gmail.com>
2026-06-08 19:59:08 -0400

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,20 +1,21 @@
     import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import SolarFlashFull        from './SolarFlashFull.jsx'
-import ReportCard             from './src/ReportCard.jsx'
-import Dashboard              from './src/Dashboard.jsx'
-import Litepaper              from './src/Litepaper.jsx'
-import SmartAlerts            from './src/SmartAlerts.jsx'
-import Ecosystem              from './src/Ecosystem.jsx'
-import AppHub                 from './src/AppHub.jsx'
-import TokenIntelligence      from './src/TokenIntelligence.jsx'
-import NarrativeRadar         from './src/NarrativeRadar.jsx'
-import SurvivalScorePage      from './src/OrbitMetrics.jsx'
-import ThreatScannerPage      from './src/ThreatScanner.jsx'
-import SolarPulsePage         from './src/SolarPulse.jsx'
-import IntelligencePanelsDemo from './src/IntelligencePanels.jsx'
-import ReactorDemo            from './src/SurvivalReactor.jsx'
+import SolarFlashFull           from './SolarFlashFull.jsx'
+import ReportCard                from './src/ReportCard.jsx'
+import Dashboard                 from './src/Dashboard.jsx'
+import Litepaper                 from './src/Litepaper.jsx'
+import SmartAlerts               from './src/SmartAlerts.jsx'
+import Ecosystem                 from './src/Ecosystem.jsx'
+import AppHub                    from './src/AppHub.jsx'
+import TokenIntelligence         from './src/TokenIntelligence.jsx'
+import NarrativeRadar            from './src/NarrativeRadar.jsx'
+import SmartMoneyIntelligence    from './src/SmartMoneyIntelligence.jsx'
+import SurvivalScorePage         from './src/OrbitMetrics.jsx'
+import ThreatScannerPage         from './src/ThreatScanner.jsx'
+import SolarPulsePage            from './src/SolarPulse.jsx'
+import IntelligencePanelsDemo    from './src/IntelligencePanels.jsx'
+import ReactorDemo               from './src/SurvivalReactor.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -24,6 +25,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/app"           element={<AppHub />} />
         <Route path="/token"         element={<TokenIntelligence />} />
         <Route path="/narrative"     element={<NarrativeRadar />} />
+        <Route path="/smart-money"   element={<SmartMoneyIntelligence />} />
         <Route path="/report"        element={<ReportCard />} />
         <Route path="/dashboard"     element={<Dashboard />} />
         <Route path="/litepaper"     element={<Litepaper />} />
